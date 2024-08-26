09f7913f429436bade380b676f14ea595c490f38
giullianacestari <dev.giullianacestari@gmail.com>
2024-08-26 11:38:41 -0300

    atualização nos laços "for"

--- script.js
+++ script.js
@@ -4,25 +4,23 @@ botaoProcessarTexto.addEventListener("click", function () {
   let texto = document.getElementById("textoEntrada").value;
   let palavras = texto.split(/\s+/);
 
-  for (let i = 0; i < palavras.length; i++) {
+  for (let i in palavras) {
     palavras[i] = palavras[i].toLowerCase();
   }
 
   const frequencia = [];
   //console.log(palavras.length);
 
-  for (let i = 0; i < palavras.length; i++) {
+  for (let i in palavras) {
     let palavra = palavras[i];
-    if (palavra) {
-      //console.log(palavra)
-      if (frequencia[palavra]) {
-        frequencia[palavra]++;
-      } else {
-        frequencia[palavra] = 1;
-      }
+    //console.log(palavra)
+    if (frequencia[palavra]) {
+      frequencia[palavra]++;
+    } else {
+      frequencia[palavra] = 1;
     }
   }
-  //console.log(frequencia);
+  console.log(frequencia);
 
   let resultado = document.getElementById("resultado");
   resultado.textContent = palavras.join(", ");
