e7f14e1083dbc291c24635be686d78f22bc9bc22
giullianacestari <dev.giullianacestari@gmail.com>
2024-08-26 15:56:23 -0300

    extraindo funções e correção de laços for

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Busca o botão
 let botaoProcessarTexto = document.getElementById("processarTexto");
-
 botaoProcessarTexto.addEventListener("click", function () {
   let textoEntrada = document.getElementById("textoEntrada").value;
   let palavrasChave = pegaPalavrasChave(textoEntrada);
@@ -8,7 +7,48 @@ botaoProcessarTexto.addEventListener("click", function () {
   resultado.textContent = palavrasChave.join(", ");
 });
 
-// Palavras ruins - preposições, artigos, etc
+function pegaPalavrasChave(texto) {
+  // Quebra o texto em palavras com regex
+  let palavras = texto.split(/\P{L}+/u);
+
+  // Converte todas as palavras para minúsculas
+  for (let i in palavras) {
+    palavras[i] = palavras[i].toLowerCase();
+  }
+
+  tiraPalavrasRuins(palavras);
+
+  agrupaPalavras();
+
+  contaFrequencia(palavrasBoas);
+
+  // Mostra as palavras ordenadas
+  let palavrasOrdenadas = [];
+  for (let palavra in frequencia) {
+    palavrasOrdenadas.push([palavra, frequencia[palavra]]);
+  }
+  palavrasOrdenadas.sort(ordenaPalavra);
+
+  // Pega as 10 palavras mais frequentes
+  palavrasOrdenadas = palavrasOrdenadas.slice(0, 10);
+
+  // Se há palavra agrupada entre as 10 mais frequentes:
+  if (palavrasOrdenadas.includes(palavrasBoas)) {
+    // Volta a palavra pra forma descrita em palavrasAgrupadas
+    palavrasOrdenadas = palavrasAgrupadas;
+  }
+
+  // Pega apenas as palavras-chave
+  let resultado = [];
+
+  for (let i in palavrasOrdenadas) {
+    resultado.push(palavrasOrdenadas[i][0]);
+  }
+
+  return resultado;
+}
+
+// Palavras ruins
 const palavrasRuins = new Set([
   "de",
   "a",
@@ -138,8 +178,34 @@ const palavrasRuins = new Set([
   "estávamos",
   "estavam",
 ]);
+// Remove as palavras ruins
+let palavrasBoas = [];
+function tiraPalavrasRuins(palavras) {
+  for (let palavra of palavras) {
+    if (!palavrasRuins.has(palavra) && palavra.length > 2) {
+      palavrasBoas.push(palavra);
+    }
+  }
+}
 
-// Palavras com o mesmo significado
+// Ordena a palavra por frequência
+function ordenaPalavra(a, b) {
+  return b[1] - a[1];
+}
+
+// Conta a frequência de cada palavra
+const frequencia = [];
+function contaFrequencia() {
+  for (let palavra of palavrasBoas) {
+    if (frequencia[palavra]) {
+      frequencia[palavra]++;
+    } else {
+      frequencia[palavra] = 1;
+    }
+  }
+}
+
+// Agrupa palavras com o mesmo significado
 const palavrasAgrupadas = {
   tecnológic: "tecnologia",
   tecnologi: "tecnologia",
@@ -148,72 +214,15 @@ const palavrasAgrupadas = {
   alun: "aluno",
   // adicionar mais e reconhecer limites dessa alternativa
 };
-
-function pegaPalavrasChave(texto) {
-  // Quebra o texto em palavras com regex
-  let palavras = texto.split(/\P{L}+/u);
-
-  // Converte todas as palavras para minúsculas
-  for (let i in palavras) {
-    palavras[i] = palavras[i].toLowerCase();
-  }
-
-  // Remove as palavras ruins
-  let palavrasBoas = [];
-  for (let i in palavras) {
-    if (!palavrasRuins.has(palavras[i]) && palavras[i].length > 2) {
-      palavrasBoas.push(palavras[i]);
-    }
-  }
-
-  // Aplica stemming e agrupa palavras de mesmo significado
-  for (let i in palavrasBoas) {
-    let palavraReduzida = palavrasBoas[i];
-
-    // Se a palavra reduzida está no grupo de palavras agrupadas (de mesmo significado):
-    if (palavrasAgrupadas[palavraReduzida]) {
-      palavrasBoas[i] = palavrasAgrupadas[palavraReduzida];
-    } else {
-      palavrasBoas[i] = palavraReduzida;
-    }
-  }
-
-  // Conta a frequência de cada palavra
-  const frequencia = {};
+function agrupaPalavras() {
   for (let i in palavrasBoas) {
     let palavra = palavrasBoas[i];
-    if (frequencia[palavra]) {
-      frequencia[palavra]++;
+
+    // Se a palavra está no grupo de palavras agrupadas (de mesmo significado):
+    if (palavrasAgrupadas[palavra]) {
+      palavrasBoas[i] = palavrasAgrupadas[palavra];
     } else {
-      frequencia[palavra] = 1;
+      palavrasBoas[i] = palavra;
     }
   }
-
-  // Ordena as palavras por frequência
-  let palavrasOrdenadas = [];
-  for (let palavra in frequencia) {
-    palavrasOrdenadas.push([palavra, frequencia[palavra]]);
-  }
-  palavrasOrdenadas.sort(function (a, b) {
-    return b[1] - a[1];
-  });
-
-  // Pega as 10 palavras mais frequentes
-  palavrasOrdenadas = palavrasOrdenadas.slice(0, 10);
-
-  // Se há palavra radicalizada entre as 10 mais frequentes:
-  if (palavrasOrdenadas.includes(palavrasBoas)) {
-    // Volta a palavra pra forma original, descrita em palavrasAgrupadas
-    palavrasOrdenadas = palavrasAgrupadas;
-  }
-
-  // Pega apenas as palavras-chave
-  let resultado = [];
-
-  for (let i in palavrasOrdenadas) {
-    resultado.push(palavrasOrdenadas[i][0]);
-  }
-
-  return resultado;
 }
-
