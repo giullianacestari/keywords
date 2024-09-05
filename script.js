72f11c2d5eb261a79acd3780e562c72383d52535
giullianacestari <dev.giullianacestari@gmail.com>
2024-09-05 11:30:53 -0300

    atualização aula3

--- script.js
+++ script.js
@@ -2,40 +2,25 @@ let botaoProcessarTexto = document.getElementById("processarTexto");
 
 botaoProcessarTexto.addEventListener("click", function () {
   let texto = document.getElementById("textoEntrada").value;
-  let palavras = texto.split(/\s+/);
+  let palavras = texto.split(/\P{L}+/u);
 
   for (let i in palavras) {
     palavras[i] = palavras[i].toLowerCase();
   }
 
   const frequencia = [];
+  //console.log(palavras.length);
 
   for (let i in palavras) {
     let palavra = palavras[i];
+    //console.log(palavra)
     if (frequencia[palavra]) {
       frequencia[palavra]++;
     } else {
       frequencia[palavra] = 1;
     }
   }
-
-  // Ordena as palavras por frequência
-  let palavrasOrdenadas = [];
-  for (let palavra in frequencia) {
-    palavrasOrdenadas.push([palavra, frequencia[palavra]]);
-  }
-  palavrasOrdenadas.sort(function (a, b) {
-    return b[1] - a[1];
-  });
-
-  // Pega as 10 palavras mais frequentes
-  palavrasOrdenadas = palavrasOrdenadas.slice(0, 10);
-
-  // Se há palavra radicalizada entre as 10 mais frequentes:
-  if (palavrasOrdenadas) {
-    // Volta a palavra pra forma original, descrita em palavrasAgrupadas
-    palavrasOrdenadas = palavras;
-  }
+  console.log(frequencia);
 
   let resultado = document.getElementById("resultado");
   resultado.textContent = palavras.join(", ");
