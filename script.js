f331f9d84d4f60a59f2e19fc9104c865ce72bc29
giullianacestari <dev.giullianacestari@gmail.com>
2024-09-24 18:07:18 -0300

    refatoração de código - aula 4

--- script.js
+++ script.js
@@ -1,33 +1,13 @@
-let botaoProcessarTexto = document.getElementById("processarTexto");
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
-botaoProcessarTexto.addEventListener("click", function () {
-  let texto = document.getElementById("textoEntrada").value;
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+  let texto = document.querySelector('#entrada-de-texto').value;
+
+  let campoResultado = document.querySelector('#resultado-palavrachave');
+
   let palavras = texto.split(/\P{L}+/u);
 
-  for (let i in palavras) {
-    palavras[i] = palavras[i].toLowerCase();
-  }
-
-  const frequencias = {};
-  //console.log(palavras.length);
-
-  for (let i in palavras) {
-    let palavra = palavras[i];
-    //console.log(palavra)
-    if (frequencias[palavra]) {
-      frequencias[palavra]++;
-    } else {
-      frequencias[palavra] = 1;
-    }
-  }
-
-  // Ordena a palavra por frequência
-  function ordenaPalavra(p1, p2) {
-    return frequencias[p2] - frequencias[p1];
-  }
-
-  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
-
-  let resultado = document.getElementById("resultado");
-  resultado.textContent = ordenadas.slice(0, 10).join(", ");
-});
+  campoResultado.textContent = palavras.join(', ');
+}
