d5e4a59907d19306ee7987a66827d8724ed9aa58
giullianacestari <dev.giullianacestari@gmail.com>
2024-09-25 18:31:27 -0300

    refatoração - aula 4

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
   const texto = document.querySelector('#entrada-de-texto').value;
   const campoResultado = document.querySelector('#resultado-palavrachave');
-  const palavrasChave = quebraEmPalavras(texto);
+  const palavrasChave = processaTexto(texto);
 
   campoResultado.textContent = palavrasChave.join(', ');
 }
 
-function quebraEmPalavras(texto){
+function processaTexto(texto){
   let palavras = texto.split(/\P{L}+/u);
   return palavras;
 }
