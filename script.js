fab70109445806b25b6fc896d440173a73f75b61
giullianacestari <dev.giullianacestari@gmail.com>
2024-09-25 18:12:06 -0300

    refatoração de código - aula 3

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
-botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-  let texto = document.querySelector('#entrada-de-texto').value;
+  const texto = document.querySelector("#entrada-de-texto").value;
+  const campoResultado = document.querySelector("#resultado-palavrachave");
 
-  let campoResultado = document.querySelector('#resultado-palavrachave');
+  let palavras = texto.split(" ");
 
-  let palavras = texto.split(' ');
-
-  campoResultado.textContent = palavras.join(', ');
+  campoResultado.textContent = palavras.join(", ");
 }
